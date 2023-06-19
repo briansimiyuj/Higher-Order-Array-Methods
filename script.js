e689555aedab18c2e45a094c8459e55ae05309e0
Brian Simiyu <briansimiyuj@gmail.com>
2023-06-19 11:00:35 +0300

    For loop example

--- script.js
+++ script.js
@@ -24,8 +24,16 @@ const companies= [
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32]
 
 
-companies.forEach(function(company){
+let canDrink = []
 
-    console.log(company.name)
+for(let i = 0; i < ages.length; i++){
 
-})
+    if(ages[i] >= 21){
+        
+        canDrink.push(ages[i])
+
+    }
+
+}
+
+console.log(canDrink)
